1b7dbdb758bbbd18c1863cdb61fe9b95d2981646
Anton Österberg <anton.osterberg@gmail.com>
2018-05-10 20:47:31 +0200

    Added pretty printer for debug purpose.

--- src/test/kotlin/com/mantono/gym/json/SerializationTester.kt
+++ src/test/kotlin/com/mantono/gym/json/SerializationTester.kt
@@ -2,12 +2,28 @@ package com.mantono.gym.json
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import java.lang.Exception
 
 internal inline fun <reified T> serializationTest(obj: T): T
 {
 	assertTrue(mapper.canSerialize(T::class.java))
 	val serialized = mapper.writeValueAsBytes(obj)
-	val deserialized: T = mapper.readValue(serialized, T::class.java)
-	assertEquals(obj, deserialized)
-	return deserialized
+	try
+	{
+		val deserialized: T = mapper.readValue(serialized, T::class.java)
+		assertEquals(obj, deserialized)
+		return deserialized
+	}
+	catch(e: Exception)
+	{
+		prettyPrint(serialized)
+		throw e
+	}
+}
+
+fun prettyPrint(obj: ByteArray)
+{
+	val json = mapper.readValue<Any>(obj, Any::class.java)
+	val output: String = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json)
+	System.out.println(output)
 }
